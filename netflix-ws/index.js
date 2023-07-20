91801b6b9a346e3c9ce02ac4a579838272b6d8a4
fp40 <luisfelipebianchini@gmail.com>
2023-07-19 21:15:05 -0300

    routes de temporadas

--- netflix-ws/index.js
+++ netflix-ws/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
-const database = require('/src/services/database')
+const database = require('./src/services/database')
 
+const filmeRoutes = require('./src/routes/filmes.routes')
+const usuarioRoutes = require('./src/routes/usuarios.routes')
+
+///MIDDLEWARES
 app.use(bodyParser.json())
+app.use(cors())
 app.use(morgan('dev'))
-app.use('/', routes)
+
+/// ROUTES
+app.use('/', filmeRoutes)
+app.use('/usuario', usuarioRoutes)
 
 app.listen(3000, () => {
   console.log('Meu servidor está funcionando')
--- netflix-ws/src/routes/filmes.routes.js
+++ netflix-ws/src/routes/filmes.routes.js
@@ -1,6 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const _ = require('underscore')
 const Filme = require('../models/filme')
+const Temporada = require('../models/temporada')
+
+// RECUPERAR TELA HOME
+router.get('/home', async (req, res) => {
+  try {
+    // recuperar todos os filmes
+    let filmes = await Filme.find({})
+    let finalFilmes = []
+
+    // recuperando temporadas
+    for (let filme of filmes) {
+      const temporadas = await Temporada.find({
+        filme_id: filme._id,
+      })
+
+      const newFilme = { ...filme._doc, temporadas }
+      finalFilmes.push(newFilme)
+    }
+
+    // misturar resultados aleatoriamente
+    finalFilmes = _.shuffle(finalFilmes)
+
+    //filme principal
+    const principal = finalFilmes[0]
+
+    //separar em seções
+    const secoes = _.chunk(finalFilmes, 5)
+
+    res.json({ error: false, principal, secoes })
+
+    // separar em categoria
+  } catch (err) {
+    res.json({ error: true, message: err.message })
+  }
+})
 
 // RECUPERAR TODOS OS REGISTROS
 router.get('/', async (req, res) => {
